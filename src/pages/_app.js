19ebd816b347cfac341bed84f4e7d32039d3be04
Guilherme Louro <guilherme-louro@hotmail.com>
2020-01-19 14:09:10 -0300

    with global styles

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import NextApp from 'next/app'
+import GlobalStyle from './global.styles'
 
 export default class App extends NextApp {
   render() {
     const { Component, pageProps } = this.props
 
-    return <Component {...pageProps} />
+    return (
+      <>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </>
+    )
   }
 }
