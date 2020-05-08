8ed9ec12cec471f6bdba9a54477b7f4b314565a5
Guilherme Louro <guilherme-louro@hotmail.com>
2020-05-08 11:50:44 -0300

    chore: remove pwd in dev mode

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 const withPWA = require('next-pwa')
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withPWA({
   pwa: {
-    dest: 'public'
+    dest: 'public',
+    disable: !isProd
   }
 })
